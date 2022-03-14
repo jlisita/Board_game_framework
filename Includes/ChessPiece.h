1c25921ce30284a9e52ef51cb00e610813ad78da
jlisita <lisita_julien@hotmail.fr>
2022-03-14 23:31:11 +0100

    define class ChessPiece herited from Piece

---
+++ Includes/ChessPiece.h
@@ -0,0 +1,110 @@
+#ifndef CHESSPIECE_H
+#define CHESSPIECE_H
+
+#include "Piece.h"
+
+class King: public Piece
+{
+
+public:
+
+	King(int raw, int column, PieceColor color);
+
+	virtual ~King();
+
+	virtual bool isAllowedMove(int i, int j) const;
+
+	virtual void print(std::ostream& stream) const;
+
+};
+
+std::ostream& operator<<(std::ostream& stream, King& piece); 
+
+class Queen: public Piece
+{
+
+public:
+
+	Queen(int raw, int column, PieceColor color);
+
+	virtual ~Queen();
+
+	virtual bool isAllowedMove(int i, int j) const;
+
+	virtual void print(std::ostream& stream) const;
+
+};
+
+std::ostream& operator<<(std::ostream& stream, Queen& piece); 
+
+class Bishop: public Piece
+{
+
+public:
+
+	Bishop(int raw, int column, PieceColor color);
+
+	virtual ~Bishop();
+
+	virtual bool isAllowedMove(int i, int j) const;
+
+	virtual void print(std::ostream& stream) const;
+
+};
+
+std::ostream& operator<<(std::ostream& stream, Bishop& piece); 
+
+class Knight: public Piece
+{
+
+public:
+
+	Knight(int raw, int column, PieceColor color);
+
+	virtual ~Knight();
+
+	virtual bool isAllowedMove(int i, int j) const;
+
+	virtual void print(std::ostream& stream) const;
+
+};
+
+std::ostream& operator<<(std::ostream& stream, Knight& piece); 
+
+class Rook: public Piece
+{
+
+public:
+
+	Rook(int raw, int column, PieceColor color);
+
+	virtual ~Rook();
+
+	virtual bool isAllowedMove(int i, int j) const;
+
+	virtual void print(std::ostream& stream) const;
+
+};
+
+std::ostream& operator<<(std::ostream& stream, Rook& piece); 
+
+class Pawn: public Piece
+{
+
+public:
+
+	Pawn(int raw, int column, PieceColor color);
+
+	virtual ~Pawn();
+
+	virtual bool isAllowedMove(int i, int j) const;
+
+	virtual void print(std::ostream& stream) const;
+
+};
+
+std::ostream& operator<<(std::ostream& stream, Pawn& piece); 
+
+
+
+#endif
---
+++ Src/ChessPiece.cpp
@@ -0,0 +1,176 @@
+#include <iostream>
+#include <string>
+
+#include "ChessPiece.h"
+
+using namespace std;
+
+King::King(int raw, int column, PieceColor color): Piece(raw, column, color)
+{
+}
+
+King::~King()
+{
+}
+
+bool King::isAllowedMove(int i, int j) const
+{
+	 return isOneSquare(i,j);
+}
+
+void King::print(std::ostream& stream) const
+{
+	string unicode = (m_color == WHITE) ? "\u2654" : "\u265A";
+	stream << unicode;
+}
+
+std::ostream& operator<<(std::ostream& stream, King& piece)
+{
+	piece.print(stream);
+	return stream;
+}
+
+
+Queen::Queen(int raw, int column, PieceColor color): Piece(raw, column, color)
+{
+}
+
+Queen::~Queen()
+{
+}
+
+bool Queen::isAllowedMove(int i, int j) const
+{
+	 return isDiagonal(i,j) || isLateral(i,j);
+}
+
+void Queen::print(std::ostream& stream) const
+{
+
+	string unicode = (m_color == WHITE) ? "\u2655" : "\u265B";
+	stream << unicode;
+}
+
+std::ostream& operator<<(std::ostream& stream, Queen& piece)
+{
+	piece.print(stream);
+	return stream;
+}
+
+Bishop::Bishop(int raw, int column, PieceColor color): Piece(raw, column, color)
+{
+}
+
+Bishop::~Bishop()
+{
+}
+
+bool Bishop::isAllowedMove(int i, int j) const
+{
+	 return isDiagonal(i,j);
+}
+
+void Bishop::print(std::ostream& stream) const
+{
+
+	string unicode = (m_color == WHITE) ? "\u2657" : "\u265D";
+	stream << unicode;
+}
+
+std::ostream& operator<<(std::ostream& stream, Bishop& piece)
+{
+	piece.print(stream);
+	return stream;
+}
+
+Knight::Knight(int raw, int column, PieceColor color): Piece(raw, column, color)
+{
+}
+
+Knight::~Knight()
+{
+}
+
+bool Knight::isAllowedMove(int i, int j) const
+{
+	return ((i==2) && ((j==1) || (j==-1))) || ( (i==-2) && ((j==1) || (j==-1))) || ((i==1) && ((j==2) || (j==-2))) || ((i==-1) && ((j==2) || (j==-2))) ;
+}
+
+void Knight::print(std::ostream& stream) const
+{
+
+	string unicode = (m_color == WHITE) ? "\u2658" : "\u265E";
+	stream << unicode;
+}
+
+std::ostream& operator<<(std::ostream& stream, Knight& piece)
+{
+	piece.print(stream);
+	return stream;
+}
+
+Rook::Rook(int raw, int column, PieceColor color): Piece(raw, column, color)
+{
+}
+
+Rook::~Rook()
+{
+}
+
+bool Rook::isAllowedMove(int i, int j) const
+{
+	 return isLateral(i,j);
+}
+
+void Rook::print(std::ostream& stream) const
+{
+
+	string unicode = (m_color == WHITE) ? "\u2656" : "\u265C";
+	stream << unicode;
+}
+
+std::ostream& operator<<(std::ostream& stream, Rook& piece)
+{
+	piece.print(stream);
+	return stream;
+}
+
+Pawn::Pawn(int raw, int column, PieceColor color): Piece(raw, column, color)
+{
+}
+
+Pawn::~Pawn()
+{
+}
+
+bool Pawn::isAllowedMove(int i, int j) const
+{
+	if(getColor() == WHITE)
+	{
+		return (i==1) && (j==0);
+	}
+	else
+	{
+		return (i==-1) && (j==0);
+	}
+}
+
+void Pawn::print(std::ostream& stream) const
+{
+
+	string unicode = (m_color == WHITE) ? "\u2659" : "\u265F";
+	stream << unicode;
+}
+
+std::ostream& operator<<(std::ostream& stream, Pawn& piece)
+{
+	piece.print(stream);
+	return stream;
+}
+
+
+
+
+
+
+
